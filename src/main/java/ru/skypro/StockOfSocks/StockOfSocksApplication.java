8b513340327e463492c96823d6121e717adc46c3
speliy <dmitriypospelov93@gmail.com>
2023-03-28 23:10:04 +0700

    Камит в котором: 1. Подключение зависимостей 2. Инцициализация структуры проекта

---
+++ src/main/java/ru/skypro/StockOfSocks/StockOfSocksApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.StockOfSocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOfSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOfSocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/StockOfSocks/StockOfSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.StockOfSocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOfSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
